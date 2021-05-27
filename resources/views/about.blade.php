5315bb566a2c413993c2ce87aa277fdba98ccd52
kangnikol <nicholasalvisaputra@gmail.com>
2021-05-27 15:41:15 +0700

    optimize About page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,17 +5,16 @@
 @section('container')
 
 <div class="container">
-  <div class="container">
-    <div class="container">
+  <div class="row-3">
+    <div class="mt-3">
       <h1>About me!</h1>
-      
-      <div class="card" style="width: 18rem;">
+      <div class="card mt-3" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title">{{$nama}}</h5><br>
           <h6 class="card-title text-muted">Trainee on PT. Senja Solusi</h6>
           <p class="card-text">Work here I'm realise that programming is pretty hard and fun!</p>
         </div>
-      </div>  
+      </div>
     </div>
   </div>
 </div>
