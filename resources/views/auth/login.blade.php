9b8dc9db159c6ca96a7ea633df5b06d9eb1accb3
kangnikol <nicholasalvisaputra@gmail.com>
2021-05-28 09:39:45 +0700

    removed forgot password feature

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,7 +51,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="form-group row mb-0 d-flex justify-content-center align-items-center h-100">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
@@ -60,12 +60,12 @@
                                 <a href="/register" class="btn btn-info">
                                     {{ __('Register') }}
                                 </button>
+                            </a>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                                {{-- @if (Route::has('password.request'))
+                                    {{-- <a class="btn btn-link" href="{{ route('password.request') }}"> --}}
+                                        {{-- {{ __('Forgot Your Password?') }} --}}
+                                {{-- @endif --}}
                             </div>
                         </div>
                     </form>
